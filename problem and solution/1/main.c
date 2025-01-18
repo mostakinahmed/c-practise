6ee0613c1eb68f9341439eb8c8054435d8930b80
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-01-18 14:07:37 +0600

    Add files via upload

---
+++ problem and solution/1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int i=1,n;
+    scanf("%d",&n);
+    while(i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+
+    return 0;
+}

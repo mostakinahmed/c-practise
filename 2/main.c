acf7fad236ef30669f19c9c0dfb9668420d86b26
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-02-04 21:58:05 +0600

    Add files via upload

---
+++ 2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ //QN: Find out if the input is an alphabet using conditional operator
+
+char x;
+printf("Enter Character: ");
+scanf("%c", &x);
+((x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z'))
+     ?  printf("%c is an alphabet.\n", x)
+     :  printf("%c is not an alphabet.\n", x);
+return 0;
+}

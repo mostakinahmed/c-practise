91f0e7651cb001d11f199a7c71781c9dc9201f41
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-02-27 19:42:12 +0600

    Add files via upload

---
+++ 33/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,n,i=1;
+    printf("Enter Num = ");
+    scanf("%d",&n);
+    while(i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+
+    return 0;
+}

4feda31e8580c043b3b8e2f00dbe57c37a3b370f
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-03-14 21:12:13 +0600

    Create simple io 3x3 .c

---
+++ Array in C/2-D/simple io 3x3 .c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3][3];
+    //simple input 3x3
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("Arr[%d][%d] = ",i,j);
+            scanf("%d",&arr[i][j]);
+      }
+      printf("\n");
+    }
+    //simple output  3x3
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf(" %d ",arr[i][j]);
+      }
+      printf("\n");
+    }
+   
+
+    return 0;
+}

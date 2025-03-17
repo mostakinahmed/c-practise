ee132cbd56baec746542d6bd8c5c591461f3851b
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-03-17 08:06:42 +0600

    Create reverse data .c

---
+++ Array in C/reverse data .c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+   int i, n, a[100];
+   
+   printf("Input the number of elements to store in the array :");
+   scanf("%d", &n);
+  
+   printf("Input %d number of elements in the array :\n", n);
+   for (i = 0; i < n; i++)
+   {
+      printf("element - %d : ", i);
+      scanf("%d", &a[i]);  // Read the input and store it in the array
+   }
+   // Display the values stored in the array
+   printf("\nThe values stored in the array are : \n");
+   for (i = 0; i < n; i++)
+   {
+       printf("% 5d", a[i]);  // Print each element in the array
+   }
+   // Display the values stored in the array in reverse order
+   printf("\n\nThe values stored in the array in reverse are :\n");
+   for (i = n - 1; i >= 0; i--)
+   {
+       printf("% 5d", a[i]);  // Print each element in reverse order
+   }
+   printf("\n\n");
+   return 0;
+}

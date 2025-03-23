659f3114b445450d674eb11c1673bac8538ddc68
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-03-23 22:53:03 +0600

    Create Length .c

---
+++ String in c/Length .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[]="Mostakin Ahmed ki obostha";
+    int len = strlen(name);
+   
+    printf("%d\n",len);
+ 
+
+    return 0;
+}

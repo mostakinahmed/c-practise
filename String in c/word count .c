903eb059797b84c69f4fd59e59b3c608634f4736
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-03-24 10:04:10 +0600

    Create word count .c

---
+++ String in c/word count .c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    int i, word_count = 0;
+    int in_word = 0;
+
+    // Use fgets instead of gets
+    printf("Enter a string: ");
+    fgets(str, sizeof(str, stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' || str[i] == '\n') {
+                word_count++;
+            }
+        } 
+
+    printf("Word count: %d\n", word_count);
+
+    return 0;
+}

44d2ffdece272c699ee93bf396110aeb9100b654
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-02 00:26:00 +0600

    Update 1 .c

--- SPL Final  LAB/1 .c
+++ SPL Final  LAB/1 .c
@@ -1,1 +1,12 @@
-mm
+
+#include <stdio.h>
+
+int main() {
+    printf("Student Id: 242-35-831\n");
+    printf("Name: Mostakin Ahmed.\n");
+    printf("Department: Software Engineering.\n");
+    printf("University Name: Daffodil International University.\n");
+   
+
+    return 0;
+}

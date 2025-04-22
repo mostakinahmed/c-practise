1e0331f2201edb8a22cc353408d72591ee75402c
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-22 23:59:44 +0600

    Create 1 (b) .c

---
+++ Fall 2024/1 (b) .c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    char data1[200], data2[100];
+
+    gets(data1);
+    gets(data2);
+
+    //find size of second data
+    int size1 = 0;
+    while (data1[size1] != '\0') {
+        size1++;
+    }
+
+    //find size of second data
+    int size2 = 0;
+    while (data2[size2] != '\0') {
+        size2++;
+    }
+
+    // Concatenate manually
+    int i = 0;
+    while (data2[i] != '\0') {
+        data1[size1] = data2[i];
+        size1++;
+        i++;
+    }
+
+    int finalSize=0;
+    while (data1[finalSize] != '\0') {
+        finalSize++;
+    }
+
+    // Output
+    printf("\n%s", data1);
+    printf("\nSize of Final string: %d", finalSize);
+
+    return 0;
+}

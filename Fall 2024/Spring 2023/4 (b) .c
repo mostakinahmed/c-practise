bf0459d1a16c5f9cae058f88eb3505dd7c9667ce
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-23 12:50:18 +0600

    Update 4 (b) .c

--- Fall 2024/Spring 2023/4 (b) .c
+++ Fall 2024/Spring 2023/4 (b) .c
@@ -1,18 +1,25 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
-int main() {
-    // Write C code here
-       int f=0,s=1,n,size;
-       printf("Enter N: ");
-       scanf("%d",&size);
-       
-       printf("0 1 ");
+int fibSeries(int size);
+
+int fibSeries(int size){
+     int f=0,s=1,n;
+     printf("0 1 ");
        for(int i=3;i<=size;i++){
           n=f+s;
           printf("%d ",n);
           f=s;
           s=n;
     }
+}
+int main() {
+    // Write C code here
+       int size;
+       printf("Enter N: ");
+       scanf("%d",&size);
+       
+       fibSeries(size);
+       
    return 0;
 }

6f4d739d826df4c06ed8476f672f69ca73c53b42
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-27 17:38:37 +0600

    Create bitwise .c

---
+++ Operator/bitwise .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int x = 5;
+    int y = 10;
+
+    printf("Or : %d\n", x | y); // bitwise OR operation
+    printf("And : %d\n", x & y); // bitwise AND operation
+    return 0;
+}

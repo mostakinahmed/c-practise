2cd244fb5e02b4056a9155b51ff6768ee41bfdef
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-05-04 16:41:16 +0600

    Create 2 -b .c

---
+++ Fall 2024/Spring 2025/2 -b .c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int isPrime(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return false;
+    }
+    if (n == 2)
+    {
+        return true;
+    }
+
+    // prime cheak
+    for (int i = 2; i < n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            return false; // if not prime
+            break;
+        }
+    }
+    //if prime
+    return true;
+}
+
+int main()
+{
+
+    int n, result;
+    printf("\nEnter a num : ");
+    scanf("%d", &n);
+
+    // call function and pass n.
+    // function return result store into result variable ( true = 1, false = 0 )
+    result = isPrime(n);
+    if (result == 1)
+    {
+        printf("Prime");
+    }
+    else if (result == 0)
+    {
+        printf("Not Prime");
+    }
+
+    // if (isPrime(n) == 1)
+    // {
+    //     printf("Prime");
+    // }
+    // else if (isPrime(n) == 0)
+    // {
+    //     printf("Not Prime");
+    // }
+
+    return 0;
+}

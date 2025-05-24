0718fc211a42767fd0d6650f4d082c349dfe00ef
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-05-24 19:59:08 +0600

    Update 4 .c

--- Tajnin/B/4 .c
+++ Tajnin/B/4 .c
@@ -8,7 +8,7 @@ int main() {
     gets(str);
 
     for (int i = 0; str[i] != '\0'; i++) {
-        if (isupper(str[i])){
+       if (str[i] >= 'A' && str[i] <= 'Z'){
             str[i]=str[i]+1; //change to next case
         }
     }

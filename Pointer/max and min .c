8ebc64407e6ce09a0ee0ccdb7efe59d7ea2b2f68
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-05-28 19:39:38 +0600

    Create max and min .c

---
+++ Pointer/max and min .c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int data[100], n;
+    int *ptr;
+    ptr = data;
+    printf("Enter Size: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("data[%d]: ", i);
+        scanf("%d", &data[i]);
+    }
+
+    int max = *ptr;
+    int min = *ptr;
+    for (int i = 1; i < n; i++)
+    {
+        ptr++;
+        // min
+        if (*ptr < min)
+        {
+            min = *ptr;
+        }
+        // max
+        if (*ptr > max)
+        {
+            max = *ptr;
+        }
+    }
+
+    printf("max = %d\n", max);
+    printf("min = %d", min);
+    return 0;
+}
